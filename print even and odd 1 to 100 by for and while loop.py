036726074adfe7744fb7b5f67c707be2ef35f02a
mr.anand <madhurendraanand5@gmail.com>
2024-11-03 00:05:44 +0530

    Add files via upload

---
+++ print even and odd 1 to 100 by for and while loop.py
@@ -0,0 +1,24 @@
+#to print even  number from 1 to 100 using "for" loop
+for x in range(2,100,2):
+    print(x )
+
+
+
+# to print odd number from 1 to 100 using "for" loop
+for x in range(1,100,2):
+    print(x )
+
+
+# to print all even number fromn 1 to 100 using "while loop"
+x= 2
+while x<=100:
+  print(x , end = " ")
+  x = x+2
+
+
+
+#to print all odd number from 1 to 100 using while loop
+y=1
+while y<100:
+    print(y , end =" ")
+    y=y+2
